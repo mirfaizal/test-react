0e584cf21232cdab24b91f66737e40b8f2cdd549
faizal mir <faizal.mir@libertymutual.com>
2018-06-27 16:25:16 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Game/>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+    render(){
+        return(
+            <div className="container">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Play Nine</h1>
+                </header>
+                <div className="row">
+                    <Stars/>
+                    <Button/>
+                    <Answer/>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Stars extends React.Component {
+    render(){
+        return(
+            <div className="col-5">
+                 <i className="fa fa-star fa-spinner">no star</i>
+                 <i className="fa fa-star fa-spinner">no star</i>
+                 <i className="fa fa-star fa-spinner">no star</i>
+            </div>
+        );
+    }
+}
+
+class Button extends React.Component {
+    render(){
+        return(
+            <div className="col-2">
+                <button>=</button>
+            </div>
+        );
+    }
+}
+
+class Answer extends React.Component {
+    render(){
+        return(
+            <div className="col-5"><font-awesome-icon icon="spinner" rotation="90" /></div>
+        );
+    }
+}
+
+export default App;
